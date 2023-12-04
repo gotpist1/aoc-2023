6834a8f15cfb0dd3c3b3adbf3ec13c85ecc56d06
pstridsb1 <pierre.stridsberg@astrazeneca.com>
2023-12-04 15:05:44 +0100

    Refactor adjacent method

--- day03/src/aoc.py
+++ day03/src/aoc.py
@@ -1,14 +1,6 @@
 import re
-from itertools import groupby
+import string
 from os import environ
-
-
-class Symbol:
-    def __init__(self, value, idx, row, line_length):
-        self.value = value
-        self.idx = idx
-        self.row = row
-        self.line_length = line_length
 
 
 class Number:
@@ -20,81 +12,46 @@ class Number:
         self.line_length = line_length
         self.adjacent_key = None
 
-    def is_adjacent(self, symbol_index_map):
-        key = str(self.row) + "-" + str(self.start_idx)
-        left_key = str(self.row) + "-" + str(self.start_idx - 1)
-        right_key = str(self.row) + "-" + str(self.end_idx + 1)
-        down_key = str(self.row + 1) + "-" + str(self.start_idx)
-        down_key_end = str(self.row + 1) + "-" + str(self.end_idx)
-        down_key_middle = str(self.row + 1) + "-" + str(self.start_idx + 1)
-        down_key_left = str(self.row + 1) + "-" + str(self.start_idx - 1)
-        down_key_right = str(self.row + 1) + "-" + str(self.end_idx + 1)
-        up_key = str(self.row - 1) + "-" + str(self.start_idx)
-        up_key_end = str(self.row - 1) + "-" + str(self.end_idx)
-        up_key_middle = str(self.row - 1) + "-" + str(self.start_idx + 1)
-        up_key_left = str(self.row - 1) + "-" + str(self.start_idx - 1)
-        up_key_right = str(self.row - 1) + "-" + str(self.end_idx + 1)
-        if key in symbol_index_map.keys():
-            self.adjacent_key = key
-            return True
-        elif left_key in symbol_index_map.keys():
-            self.adjacent_key = left_key
-            return True
-        elif right_key in symbol_index_map.keys():
-            self.adjacent_key = right_key
-            return True
-        elif down_key in symbol_index_map.keys():
-            self.adjacent_key = down_key
-            return True
-        elif down_key_end in symbol_index_map.keys():
-            self.adjacent_key = down_key_end
-            return True
-        elif down_key_left in symbol_index_map.keys():
-            self.adjacent_key = down_key_left
-            return True
-        elif down_key_right in symbol_index_map.keys():
-            self.adjacent_key = down_key_right
-            return True
-        elif down_key_middle in symbol_index_map.keys():
-            self.adjacent_key = down_key_middle
-            return True
-        elif up_key in symbol_index_map.keys():
-            self.adjacent_key = up_key
-            return True
-        elif up_key_end in symbol_index_map.keys():
-            self.adjacent_key = up_key_end
-            return True
-        elif up_key_left in symbol_index_map.keys():
-            self.adjacent_key = up_key_left
-            return True
-        elif up_key_right in symbol_index_map.keys():
-            self.adjacent_key = up_key_right
-            return True
-        elif up_key_middle in symbol_index_map.keys():
-            self.adjacent_key = up_key_middle
-            return True
-        else:
-            return False
+    def is_adjacent(self, symbol_index_map) -> bool:
+        same_row_hit = self.get_adjacent_key(self.row, symbol_index_map)
+        down_hit = self.get_adjacent_key(self.row + 1, symbol_index_map)
+        up_hit = self.get_adjacent_key(self.row - 1, symbol_index_map)
+        return up_hit or down_hit or same_row_hit
+
+    def get_adjacent_key(self, key, symbol_index_map) -> bool:
+        y = [
+            self.start_idx,
+            self.end_idx,
+            self.start_idx + 1,
+            self.start_idx - 1,
+            self.end_idx + 1
+
+        ]
+        for value in y:
+            coordinate = str(key) + "-" + str(value)
+            if coordinate in symbol_index_map.keys():
+                self.adjacent_key = coordinate
+                return True
 
 
-def get_symbol_index_map(scrubbed):
+def get_symbol_index_map(scrubbed) -> dict[string, string]:
     symbol_index_map = {}
     r1 = re.compile(r'[^.\d+]$')
     r2 = re.compile(r'[a-zA-Z_#@:;\\/\\\\=!^() +-]+')
     for row_idx, line in enumerate(scrubbed):
         for idx, char in enumerate(line):
             if r1.match(char) or r2.match(char):
-                symbol_index_map[str(row_idx) + "-" + str(idx)] = Symbol(char, idx, row_idx, len(line))
+                symbol_index_map[str(row_idx) + "-" + str(idx)] = char
     return symbol_index_map
 
 
-def getNumber(startIdx, line, row):
+def get_number(start_idx, line, row) -> Number:
     curr_idx = 0
-    for idx in range(startIdx, len(line)):
+    for idx in range(start_idx, len(line)):
         curr_idx = idx
         if not line[idx].isdigit() or idx == len(line):
-            return Number(line[startIdx:idx], startIdx, idx - 1, row, len(line))
-    return Number(line[startIdx:curr_idx + 1], startIdx, curr_idx - 1, row, len(line))
+            return Number(line[start_idx:idx], start_idx, idx - 1, row, len(line))
+    return Number(line[start_idx:curr_idx + 1], start_idx, curr_idx - 1, row, len(line))
 
 
 def get_valid_numbers(scrubbed):
@@ -103,7 +60,7 @@ def get_valid_numbers(scrubbed):
         number = None
         for idx, char in enumerate(line):
             if char.isdigit() and number is None or char.isdigit() and idx > number.end_idx:
-                number = getNumber(idx, line, row_idx)
+                number = get_number(idx, line, row_idx)
                 numbers.append(number)
     return numbers
 
@@ -112,20 +69,20 @@ def getSolutionPart1(input_list):
     # answer 522726
     scrubbed = list(map(lambda x: x.rstrip(), input_list))
     symbol_index_map = get_symbol_index_map(scrubbed)
-    valid_numbers = [int(number.value) for number in get_valid_numbers(scrubbed) if number.is_adjacent(symbol_index_map)]
+    valid_numbers = [int(number.value) for number in get_valid_numbers(scrubbed) if
+                     number.is_adjacent(symbol_index_map)]
     return sum(valid_numbers)
 
 
 def getSolutionPart2(input_list):
     # answer 81721933
     scrubbed = list(map(lambda x: x.rstrip(), input_list))
-    gear_index_map = {key: value.value for key, value in get_symbol_index_map(scrubbed).items() if value.value == "*"}
+    gear_index_map = {key: value for key, value in get_symbol_index_map(scrubbed).items() if value == "*"}
     valid_numbers = [number for number in get_valid_numbers(scrubbed) if number.is_adjacent(gear_index_map)]
     gears_map = {value.adjacent_key: [] for value in valid_numbers}
     for n in valid_numbers:
         gears_map[n.adjacent_key].append(int(n.value))
-    gears_list = list(filter(lambda x: len(x) == 2, gears_map.values()))
-    valid_gears = list(map(lambda x: x[0] * x[1], gears_list))
+    valid_gears = list(map(lambda x: x[0] * x[1], list(filter(lambda x: len(x) == 2, gears_map.values()))))
     return sum(valid_gears)
 
 
